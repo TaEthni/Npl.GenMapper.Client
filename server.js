f4222a1830bbc09f01fe2ccd1241927e5461b4d4
Garrett Cox <garrettjcox@gmail.com>
2018-07-09 21:21:51 -0500

    Send index.html on all other routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const conditional = require('koa-conditional-get');
 const compress = require('koa-compress');
 const kstatic = require('koa-static');
 const etag = require('koa-etag');
+const send = require('koa-send');
 
 const server = new Koa();
 
@@ -11,6 +12,7 @@ server
   .use(compress())
   .use(conditional())
   .use(etag())
-  .use(kstatic(__dirname + '/dist/tools-angular'));
+  .use(kstatic('dist/tools-angular'))
+  .use((ctx) => send(ctx, 'dist/tools-angular/index.html'));
 
 server.listen(process.env.PORT || 9001);
