aca4aba7e7b239a4cd6742ad1240e6100353421b
Joel Cox <joel.cox.dev@gmail.com>
2019-01-10 13:48:59 -0600

    Auto-generated commit

--- main.js
+++ main.js
@@ -1806,10 +1806,10 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 
 var _BaseUrl = 'http://localhost:9000/api/';
 if (window.location.host === 'dev.noplaceleft.tools') {
-    _BaseUrl = window.location.host + '//dev-api.noplaceleft.tools/';
+    _BaseUrl = window.location.protocol + '//dev-api.noplaceleft.tools/';
 }
 if (window.location.host === 'noplaceleft.tools') {
-    _BaseUrl = window.location.host + '//api.noplaceleft.tools/';
+    _BaseUrl = window.location.protocol + '//api.noplaceleft.tools/';
 }
 var BaseUrl = _BaseUrl;
 var EntityService = /** @class */ (function () {
