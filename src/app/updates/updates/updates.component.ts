6f558dfb061462c23abde2f1b2a8091dcd20fefa
Joel Cox <joel.cox.dev@gmail.com>
2020-01-19 17:16:29 -0600

    fix(updates): update key

--- src/app/updates/updates/updates.component.ts
+++ src/app/updates/updates/updates.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { WhatsNewDialogComponent } from '../whats-new-dialog/whats-new-dialog.component';
 import { UpdatesService } from '../updates.service';
+import { WhatsNewDialogComponent } from '../whats-new-dialog/whats-new-dialog.component';
 
 @Component({
     selector: 'app-updates',
@@ -34,7 +34,7 @@ export class UpdatesComponent implements OnInit {
             )
             .afterClosed()
             .subscribe(result => {
-                localStorage.setItem('update-v1', 'true');
+                localStorage.setItem(this.updateKey, 'true');
             });
     }
 }
