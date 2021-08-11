e4a6be3518c966fbbaa4af3f15d11dcf6c009804
Chad Griffis <16548994+gryffs@users.noreply.github.com>
2021-08-11 18:04:46 -0500

    fix(GAPP-1245): add api key references to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
     production: true,
     apiKey: 'AIzaSyCzMNmQPVY9uivoKSzoj0ACwKr-LxxcHko',
     apiBase: 'https://npl-api.genmapper.com/api/',
+    ipGeoUrl: 'https://ipgeolocation.abstractapi.com/v1/',
+    ipGeoApiKey: '',
     authConfig: {
         authority: 'https://idp.taethni.com',
         clientId: 'npl_client',
