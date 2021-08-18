5f9ef90b663ae2bd530cf1350182648d664f5422
Joel Cox <joel.cox.dev@gmail.com>
2021-08-18 11:12:03 -0500

    feat: add tracking for taethni

--- src/assets/tracking.js
+++ src/assets/tracking.js
@@ -3,9 +3,26 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-    var u = "https://genmapper.matomo.cloud/";
+    var u = "https://analytics.taethni.com/";
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.src = '//cdn.matomo.cloud/genmapper.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
+    g.type = 'text/javascript'; g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
+
+
+// < !--Matomo -->
+// <script>
+//   var _paq = window._paq = window._paq || [];
+//   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+//   _paq.push(['trackPageView']);
+//   _paq.push(['enableLinkTracking']);
+//   (function() {
+//     var u="//analytics.taethni.com/";
+//     _paq.push(['setTrackerUrl', u+'matomo.php']);
+//     _paq.push(['setSiteId', '1']);
+//     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+//     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+//   })();
+// </script>
+// <!--End Matomo Code-- >
