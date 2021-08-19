93d57310cef6f6c6c15135b4f67bd2538affbef5
Joel Cox <joel.cox.dev@gmail.com>
2021-08-18 21:59:12 -0500

    chore: update tracking

--- src/assets/tracking.js
+++ src/assets/tracking.js
@@ -1,28 +1,37 @@
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+// var _paq = window._paq = window._paq || [];
+// /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+// _paq.push(['trackPageView']);
+// _paq.push(['enableLinkTracking']);
+// (function () {
+//     var u = "https://analytics.taethni.com/";
+//     _paq.push(['setTrackerUrl', u + 'matomo.php']);
+//     _paq.push(['setSiteId', '1']);
+//     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+//     g.type = 'text/javascript'; g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+// })();
+
+var _paq = window._paq || [];
+_paq.push(["setRequestMethod", "POST"]);
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+if (window.location.hostname === "localhost") {
+    _paq.push(["setCookieDomain", "localhost"]);
+    _paq.push(["setDomains", ["localhost"]]);
+} else {
+    _paq.push(["setCookieDomain", "*.genmapper.com"]);
+    _paq.push(["setDomains", ["*.genmapper.com", "*npl.genmapper.com", "*.taethni.com"]]);
+}
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+_paq.push(['enableHeartBeatTimer']);
+if (document.referrer) {
+    _paq.push(['setReferrerUrl', document.referrer]);
+}
 (function () {
-    var u = "https://analytics.taethni.com/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    var u = "//stats.taethni.com/";
+    _paq.push(['setRequestMethod', 'POST']);
+    _paq.push(['setTrackerUrl', u + 'js/']);
     _paq.push(['setSiteId', '1']);
+
     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'js/'; s.parentNode.insertBefore(g, s);
 })();
-
-
-// < !--Matomo -->
-// <script>
-//   var _paq = window._paq = window._paq || [];
-//   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-//   _paq.push(['trackPageView']);
-//   _paq.push(['enableLinkTracking']);
-//   (function() {
-//     var u="//analytics.taethni.com/";
-//     _paq.push(['setTrackerUrl', u+'matomo.php']);
-//     _paq.push(['setSiteId', '1']);
-//     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-//     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-//   })();
-// </script>
-// <!--End Matomo Code-- >
