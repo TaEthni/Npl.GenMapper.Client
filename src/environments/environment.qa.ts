58e7ec17e3446cccbff6c6c51ad3b7603df4bbb8
joelcoxokc <joel.cox.dev@gmail.com>
2023-06-13 18:56:02 -0500

    fix: add web_api scope

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -11,7 +11,7 @@ export const environment = {
         authority: 'https://qa.myidp.io',
         clientId: 'npl_client',
         responseType: 'code',
-        scope: 'profile openid IdentityServerApi npl_api roles offline_access',
+        scope: 'profile openid IdentityServerApi web_api npl_api roles offline_access',
         logLevel: 'Error'
     }
 };
