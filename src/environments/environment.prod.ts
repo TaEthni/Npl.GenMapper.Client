c01c4c601724e487f1e12cfd3002ed643da666aa
joelcoxokc <joel.cox.dev@gmail.com>
2023-08-01 15:55:24 -0400

    fix: prod idp ref

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     ipGeoApiKey: 'c5bb07e6f04a42c680356656041a722b',
     oikosApi: 'https://api.thegapp.app/api/',
     authConfig: {
-        authority: 'https://idp.taethni.com',
+        authority: 'https://myidp.io',
         clientId: 'npl_client',
         responseType: 'code',
         scope: 'profile openid IdentityServerApi web_api npl_api roles offline_access',
